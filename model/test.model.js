b0456d2694b941ec782444ae28830cd3589dbc7d
RAMarenco <00080121@uca.edu.sv>
2023-10-15 18:33:35 -0600

    Delete: unnecesary code

--- model/test.model.js
+++
@@ -1,9 +0,0 @@
-const joi = require('joi');
-
-const testschema = joi.object({
-    name: joi.string().min(3).max(15).required(),
-    email: joi.string().email(),
-    hash: joi.string().pattern(new RegExp(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;"'<>,.?/\\|]).{8,30}$/)),
-});
-
-module.exports = testschema;
